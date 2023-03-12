0e800f29de2e78095c3e0b60d05114e38b2e0fe4
njeruthuo <njeruthelearner@gmail.com>
2023-03-12 13:05:40 +0300

    Added flights to the project.

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,3 +1,16 @@
+# from django.contrib.admin.models import Group
 from django.contrib import admin
 
-# Register your models here.
+from .models import Flight, Airport
+
+# admin.site.unregister(Group)
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    pass
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Flight
 
 
 def index(request):
-    pass
+    return render(request, 'flights/index.html', {
+        'flights': Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, 'flights/flights.html', {
+        'flight': flight
+    })
