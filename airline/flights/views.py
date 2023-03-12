919fccd37c9e85919af558b69e8e78f6a44f3169
njeruthuo <njeruthelearner@gmail.com>
2023-03-12 13:54:30 +0300

    Added a cool feature. A user can add passengers to a flight

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Flight
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
@@ -13,5 +15,20 @@ def flight(request, flight_id):
 
     return render(request, 'flights/flights.html', {
         'flight': flight,
-        'passengers':flight.passenger.all()
+        'passengers': flight.passenger.all(),
+        'non_passengers': Passenger.objects.exclude(flights = flight).all()
+    })
+
+
+def book(request, flight_id):
+    if request.method == 'POST':
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST['passenger']))
+
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse('flights:flight', args=(flight.id,)))
+
+
+
+    return render(request, 'flights/book.html', {
     })
